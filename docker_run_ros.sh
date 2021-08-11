782f01a41a77c25629546c57b9cda621b2a16b67
Eric Lu <eric565648@gmail.com>
2021-08-11 18:21:16 +0800

    minor update

--- docker_run_ros.sh
+++ docker_run_ros.sh
@@ -33,7 +33,7 @@ docker run \
     -e ROS_MASTER_URI=$ROS_MASTER_URI \
     -e ROS_IP=$ROS_IP \
     -v "$XAUTH:$XAUTH" \
-    -v "/home/$USER/robotics_middleware_trial_python_turtle_ros2:/home/rpirobotics/robotics_middleware_trial_python_turtle_ros2" \
+    -v "/home/$USER/Robotics_Middleware_Trial_Python_Turtle:/home/rpirobotics/Robotics_Middleware_Trial_Python_Turtle" \
     -v "/tmp/.X11-unix:/tmp/.X11-unix" \
     -v "/etc/localtime:/etc/localtime:ro" \
     -v "/dev:/dev" \
